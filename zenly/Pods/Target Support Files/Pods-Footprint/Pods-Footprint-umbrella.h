9f6c474bbd1c031ddf3b7b1ff0fc324a5d721319
Lanqing Cheng <lqqcheng@ucdavis.edu>
2019-12-05 00:49:04 -0800

    debuged

--- zenly/Pods/Target Support Files/Pods-Footprint/Pods-Footprint-umbrella.h
+++ zenly/Pods/Target Support Files/Pods-Footprint/Pods-Footprint-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_zenlyVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_zenlyVersionString[];
+FOUNDATION_EXPORT double Pods_FootprintVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_FootprintVersionString[];
 
--- zenly/Pods/Target Support Files/Pods-zenly/Pods-zenly-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_zenly : NSObject
-@end
-@implementation PodsDummy_Pods_zenly
-@end
